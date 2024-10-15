ac4a7913066b38c748cf5889b1b536e472984376
joszeRamirez <jramirezc9@est.ups.edu.ec>
2024-10-15 08:30:52 -0500

    kjasdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./sections/header/header.component";
 import { FooterComponent } from "./sections/footer/footer.component";
+import { AddCourseComponent } from "./components/add-course/add-course.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AddCourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
