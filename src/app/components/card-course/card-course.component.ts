3089c02fa2028118cf9504ad3d5567f7656c7e24
joszeRamirez <jramirezc9@est.ups.edu.ec>
2024-10-15 08:21:03 -0500

    first commit

---
+++ src/app/components/card-course/card-course.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-course',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-course.component.html',
+  styleUrl: './card-course.component.scss'
+})
+export class CardCourseComponent {
+
+}
---
+++ src/models/course.ts
@@ -0,0 +1,19 @@
+export class Course {
+    name 
+    category
+    duration_course
+    teaches_name
+    startDate
+    description
+    imgSrc
+
+    constructor(name : string, category: string, duration_course : number, teaches_name : string, startDate: Date, { description = "", imgSrc = "../../assets/img/course/default-img.jpg" } = {}) {
+        this.name = name;                  
+        this.category = category;           
+        this.duration_course = duration_course; 
+        this.teaches_name = teaches_name;
+        this.startDate = startDate;      
+        this.description = description;     
+        this.imgSrc = imgSrc;              
+    }
+}
